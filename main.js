dae5f2a49f05b6229800e25da96094f672c8529f
DevShayan <shayanahmed393@gmail.com>
2024-01-22 19:10:12 +0500

    Changes to be committed: 	modified:   index.html 	new file:   jquery.js 	modified:   main.css 	modified:   main.js

--- main.js
+++ main.js
@@ -1,218 +1,337 @@
-const creditHours1 = document.getElementById("c_hours1");
-const creditHours2 = document.getElementById("c_hours2");
-const creditHours3 = document.getElementById("c_hours3");
-const creditHours4 = document.getElementById("c_hours4");
-const creditHours5 = document.getElementById("c_hours5");
-const creditHours6 = document.getElementById("c_hours6");
+$(document).ready(main);
 
-const obtainedMarks1 = document.getElementById("obtained_subj1");
-const obtainedMarks2 = document.getElementById("obtained_subj2");
-const obtainedMarks3 = document.getElementById("obtained_subj3");
-const obtainedMarks4 = document.getElementById("obtained_subj4");
-const obtainedMarks5 = document.getElementById("obtained_subj5");
-const obtainedMarks6 = document.getElementById("obtained_subj6");
+const creditHrs1 = new Map([
+    ["pf", 4],
+    ["ict", 3],
+    ["cal1", 3],
+    ["app_ph", 3],
+    ["ecc", 3],
+    ["isl", 2]
+]);
 
-let obtainedMarks = [];
-let creditHours = [];
+const creditHrs2 = new Map([
+    ["oop", 4],
+    ["dld", 4],
+    ["ps", 3],
+    ["ur", 3],
+    ["cps", 3],
+    ["pak_st", 2]
+]);
 
-const button = document.getElementById("btn_calculate");
+const creditHrs3 = new Map([
+    ["dsa", 4],
+    ["coal", 4],
+    ["diff_eq", 3],
+    ["dsc_st", 3],
+    ["tbw", 3]
+]);
 
-let display_input1 = document.getElementById("gpa");
+const creditHrs4 = new Map([
+    ["dbms", 4],
+    ["vp", 3],
+    ["la", 3],
+    ["daoa", 3],
+    ["toa", 2]
+]);
 
-button.onclick = function() {
-    clearErrors();
+const creditHrs5 = new Map([
+    ["pf", 4],
+    ["ict", 4],
+    ["cal1", 3],
+    ["app_ph", 3],
+    ["isl", 2]
+]);
 
-    if (validateInput())
-        display_input1.innerText = "GPA: "+calculateGPA();
+const creditHrs6 = new Map([
+    ["ai", 3],
+    ["cc", 3],
+    ["nsc", 3],
+    ["oose", 3],
+    ["wp", 3]
+]);
 
+const creditHrs7 = new Map([
+    ["dsc", 3],
+    ["ap", 3],
+    ["wp2", 3],
+    ["hci", 3],
+    ["pdc", 3],
+    ["fyp1", 3]
+]);
+
+const creditHrs8 = new Map([
+    ["mt", 3],
+    ["sda", 3],
+    ["fyp2", 3]
+]);
+
+var totalCHrs1 = 0;
+var qGrades1 = 0;
+
+var totalCHrs2 = 0;
+var qGrades2 = 0;
+
+var totalCHrs3 = 0;
+var qGrades3 = 0;
+
+var totalCHrs4 = 0;
+var qGrades4 = 0;
+
+var totalCHrs5 = 0;
+var qGrades5 = 0;
+
+var totalCHrs6 = 0;
+var qGrades6 = 0;
+
+var totalCHrs7 = 0;
+var qGrades7 = 0;
+
+var totalCHrs8 = 0;
+var qGrades8 = 0;
+
+
+function main() {
+    init();
+    semListeners();
 }
 
-function validateInput() {
-    if (creditHours1.value.trim().length === 0 && obtainedMarks1.value.trim().length !== 0) {
-        creditHours1.setCustomValidity("*Required");
-        creditHours1.reportValidity();
-
-        if (!(creditHours1.className.split(" ").includes("red-border")))
-            creditHours1.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours1.value.trim().length !== 0 && obtainedMarks1.value.trim().length === 0) {
-        obtainedMarks1.setCustomValidity("*Required");
-        obtainedMarks1.reportValidity();
-
-        if (!(obtainedMarks1.className.split(" ").includes("red-border")))
-            obtainedMarks1.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours2.value.trim().length === 0 && obtainedMarks2.value.trim().length !== 0) {
-        creditHours2.setCustomValidity("*Required");
-        creditHours2.reportValidity();
-
-        if (!(creditHours2.className.split(" ").includes("red-border")))
-            creditHours2.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours2.value.trim().length !== 0 && obtainedMarks2.value.trim().length === 0) {
-        obtainedMarks2.setCustomValidity("*Required");
-        obtainedMarks2.reportValidity();
-
-        if (!(obtainedMarks2.className.split(" ").includes("red-border")))
-            obtainedMarks2.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours3.value.trim().length === 0 && obtainedMarks3.value.trim().length !== 0) {
-        creditHours3.setCustomValidity("*Required");
-        creditHours3.reportValidity();
-
-        if (!(creditHours3.className.split(" ").includes("red-border")))
-            creditHours3.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours3.value.trim().length !== 0 && obtainedMarks3.value.trim().length === 0) {
-        obtainedMarks3.setCustomValidity("*Required");
-        obtainedMarks3.reportValidity();
-
-        if (!(obtainedMarks3.className.split(" ").includes("red-border")))
-            obtainedMarks3.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours4.value.trim().length === 0 && obtainedMarks4.value.trim().length !== 0) {
-        creditHours4.setCustomValidity("*Required");
-        creditHours4.reportValidity();
-
-        if (!(creditHours4.className.split(" ").includes("red-border")))
-            creditHours4.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours4.value.trim().length !== 0 && obtainedMarks4.value.trim().length === 0) {
-        obtainedMarks4.setCustomValidity("*Required");
-        obtainedMarks4.reportValidity();
-
-        if (!(obtainedMarks4.className.split(" ").includes("red-border")))
-            obtainedMarks4.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours5.value.trim().length === 0 && obtainedMarks5.value.trim().length !== 0) {
-        creditHours5.setCustomValidity("*Required");
-        creditHours5.reportValidity();
-
-        if (!(creditHours5.className.split(" ").includes("red-border")))
-            creditHours5.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours5.value.trim().length !== 0 && obtainedMarks5.value.trim().length === 0) {
-        obtainedMarks5.setCustomValidity("*Required");
-        obtainedMarks5.reportValidity();
-
-        if (!(obtainedMarks5.className.split(" ").includes("red-border")))
-            obtainedMarks5.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours6.value.trim().length === 0 && obtainedMarks6.value.trim().length !== 0) {
-        creditHours6.setCustomValidity("*Required");
-        creditHours6.reportValidity();
-
-        if (!(creditHours6.className.split(" ").includes("red-border")))
-            creditHours6.className += " red-border";
-        
-        return false;
-    }
-    if (creditHours6.value.trim().length !== 0 && obtainedMarks6.value.trim().length === 0) {
-        obtainedMarks6.setCustomValidity("*Required");
-        obtainedMarks6.reportValidity();
-
-        if (!(obtainedMarks6.className.split(" ").includes("red-border")))
-            obtainedMarks6.className += " red-border";
-        
-        return false;
-    }
-
-    creditHours = [], obtainedMarks = [];
-
-    if (creditHours1.value.trim().length !== 0)
-        creditHours.push(parseInt(creditHours1.value));
-    if (creditHours2.value.trim().length !== 0)
-        creditHours.push(parseInt(creditHours2.value));
-    if (creditHours3.value.trim().length !== 0)
-        creditHours.push(parseInt(creditHours3.value));
-    if (creditHours4.value.trim().length !== 0)
-        creditHours.push(parseInt(creditHours4.value));
-    if (creditHours5.value.trim().length !== 0)
-        creditHours.push(parseInt(creditHours5.value));
-    if (creditHours6.value.trim().length !== 0)
-        creditHours.push(parseInt(creditHours6.value));
-
-    if (obtainedMarks1.value.trim().length !== 0)
-        obtainedMarks.push(parseInt(obtainedMarks1.value));
-    if (obtainedMarks2.value.trim().length !== 0)
-        obtainedMarks.push(parseInt(obtainedMarks2.value));
-    if (obtainedMarks3.value.trim().length !== 0)
-        obtainedMarks.push(parseInt(obtainedMarks3.value));
-    if (obtainedMarks4.value.trim().length !== 0)
-        obtainedMarks.push(parseInt(obtainedMarks4.value));
-    if (obtainedMarks5.value.trim().length !== 0)
-        obtainedMarks.push(parseInt(obtainedMarks5.value));
-    if (obtainedMarks6.value.trim().length !== 0)
-        obtainedMarks.push(parseInt(obtainedMarks6.value));
-
-    if (obtainedMarks.length == 0 && creditHours.length == 0) {
-        obtainedMarks1.setCustomValidity("Atleast one input is required!");
-        obtainedMarks1.reportValidity();
-        return false;
-    }
-
-    return true;
-
+function init() {
+    setCreditHours();
+    setTotalCHrs();
 }
 
-function calculateGPA() {
-    let totalCH = 0, numerator = 0;
+function setCreditHours() {
+    $("#c_hours_pf").text(creditHrs1.get("pf"));
+    $("#c_hours_ict").text(creditHrs1.get("ict"));
+    $("#c_hours_cal1").text(creditHrs1.get("cal1"));
+    $("#c_hours_app_ph").text(creditHrs1.get("app_ph"));
+    $("#c_hours_ecc").text(creditHrs1.get("ecc"));
+    $("#c_hours_isl").text(creditHrs1.get("isl"));
 
-    for (let x=0; x<creditHours.length; x++) {
-        totalCH += creditHours[x];
+    $("#c_hours_oop").text(creditHrs2.get("oop"));
+    $("#c_hours_dld").text(creditHrs2.get("dld"));
+    $("#c_hours_ps").text(creditHrs2.get("ps"));
+    $("#c_hours_ur").text(creditHrs2.get("ur"));
+    $("#c_hours_cps").text(creditHrs2.get("cps"));
+    $("#c_hours_pak_st").text(creditHrs2.get("pak_st"));
+
+    $("#c_hours_dsa").text(creditHrs3.get("dsa"));
+    $("#c_hours_coal").text(creditHrs3.get("coal"));
+    $("#c_hours_diff_eq").text(creditHrs3.get("diff_eq"));
+    $("#c_hours_dsc_st").text(creditHrs3.get("dsc_st"));
+    $("#c_hours_tbw").text(creditHrs3.get("tbw"));
+
+    $("#c_hours_dbms").text(creditHrs4.get("dbms"));
+    $("#c_hours_vp").text(creditHrs4.get("vp"));
+    $("#c_hours_la").text(creditHrs4.get("la"));
+    $("#c_hours_daoa").text(creditHrs4.get("daoa"));
+    $("#c_hours_toa").text(creditHrs4.get("toa"));
+
+    $("#c_hours_pf").text(creditHrs5.get("pf"));
+    $("#c_hours_ict").text(creditHrs5.get("ict"));
+    $("#c_hours_cal1").text(creditHrs5.get("cal1"));
+    $("#c_hours_app_ph").text(creditHrs5.get("app_ph"));
+    $("#c_hours_ecc").text(creditHrs5.get("ecc"));
+
+    $("#c_hours_ai").text(creditHrs6.get("ai"));
+    $("#c_hours_cc").text(creditHrs6.get("cc"));
+    $("#c_hours_nsc").text(creditHrs6.get("nsc"));
+    $("#c_hours_oose").text(creditHrs6.get("oose"));
+    $("#c_hours_wp").text(creditHrs6.get("wp"));
+
+    $("#c_hours_dsc").text(creditHrs7.get("dsc"));
+    $("#c_hours_ap").text(creditHrs7.get("ap"));
+    $("#c_hours_wp2").text(creditHrs7.get("wp2"));
+    $("#c_hours_hci").text(creditHrs7.get("hci"));
+    $("#c_hours_pdc").text(creditHrs7.get("pdc"));
+    $("#c_hours_fyp1").text(creditHrs7.get("fyp1"));
+
+    $("#c_hours_mt").text(creditHrs8.get("mt"));
+    $("#c_hours_sda").text(creditHrs8.get("sda"));
+    $("#c_hours_fyp2").text(creditHrs8.get("fyp2"));
+}
+
+function setTotalCHrs() {
+    for (const value of creditHrs1.values()) {
+        totalCHrs1 += value;
     }
-
-    for (let x=0; x<obtainedMarks.length; x++) {
-        numerator += getGrade(obtainedMarks[x]) * creditHours[x];
+    for (const value of creditHrs2.values()) {
+        totalCHrs2 += value;
     }
-    
-    return numerator / totalCH;
+    for (const value of creditHrs3.values()) {
+        totalCHrs3 += value;
+    }
+    for (const value of creditHrs4.values()) {
+        totalCHrs4 += value;
+    }
+    for (const value of creditHrs5.values()) {
+        totalCHrs5 += value;
+    }
+    for (const value of creditHrs6.values()) {
+        totalCHrs6 += value;
+    }
+    for (const value of creditHrs7.values()) {
+        totalCHrs7 += value;
+    }
+    for (const value of creditHrs8.values()) {
+        totalCHrs8 += value;
+    }
 }
 
-function getGrade(marks) {
-    obj = parseInt(marks);
-    if (obj >= 80) return 4.0;
-    else if (obj >= 75) return 3.5;
-    else if (obj >= 70) return 3.0;
-    else if (obj >= 65) return 2.5;
-    else if (obj >= 55) return 2.0;
-    else if (obj >= 50) return 1.5;
-    else return 0.0;
+function getQGradesForSem(semester) {
+    switch (semester) {
+        case 1:
+            return (creditHrs1.get("pf")*$("#gp_pf").text()) +
+            (creditHrs1.get("ict")*$("#gp_ict").text()) +
+            (creditHrs1.get("cal1")*$("#gp_cal1").text()) +
+            (creditHrs1.get("app_ph")*$("#gp_app_ph").text()) +
+            (creditHrs1.get("ecc")*$("#gp_ecc").text()) +
+            (creditHrs1.get("isl")*$("#gp_isl").text());
+        case 2:
+            return (creditHrs2.get("oop")*$("#gp_oop").text()) +
+            (creditHrs2.get("dld")*$("#gp_dld").text()) +
+            (creditHrs2.get("ps")*$("#gp_ps").text()) +
+            (creditHrs2.get("ur")*$("#gp_ur").text()) +
+            (creditHrs2.get("cps")*$("#gp_cps").text()) +
+            (creditHrs2.get("pak_st")*$("#gp_pak_st").text());
+        case 3:
+            return (creditHrs3.get("dsa")*$("#gp_dsa").text()) +
+            (creditHrs3.get("coal")*$("#gp_coal").text()) +
+            (creditHrs3.get("diff_eq")*$("#gp_diff_eq").text()) +
+            (creditHrs3.get("dsc_st")*$("#gp_dsc_st").text()) +
+            (creditHrs3.get("tbw")*$("#gp_tbw").text());
+        case 4:
+            return (creditHrs4.get("dbms")*$("#gp_dbms").text()) +
+            (creditHrs4.get("vp")*$("#gp_vp").text()) +
+            (creditHrs4.get("la")*$("#gp_la").text()) +
+            (creditHrs4.get("daoa")*$("#gp_daoa").text()) +
+            (creditHrs4.get("toa")*$("#gp_toa").text());
+        case 5:
+            return (creditHrs5.get("pf")*$("#gp_pf").text()) +
+            (creditHrs5.get("ict")*$("#gp_ict").text()) +
+            (creditHrs5.get("cal1")*$("#gp_cal1").text()) +
+            (creditHrs5.get("app_ph")*$("#gp_app_ph").text()) +
+            (creditHrs5.get("ecc")*$("#gp_ecc").text());
+        case 6:
+            return (creditHrs6.get("ai")*$("#gp_ai").text()) +
+            (creditHrs6.get("cc")*$("#gp_cc").text()) +
+            (creditHrs6.get("nsc")*$("#gp_nsc").text()) +
+            (creditHrs6.get("oose")*$("#gp_oose").text()) +
+            (creditHrs6.get("wp")*$("#gp_wp").text());
+        case 7:
+            return (creditHrs7.get("dsc")*$("#gp_dsc").text()) +
+            (creditHrs7.get("ap")*$("#gp_ap").text()) +
+            (creditHrs7.get("wp2")*$("#gp_wp2").text()) +
+            (creditHrs7.get("hci")*$("#gp_hci").text()) +
+            (creditHrs7.get("pdc")*$("#gp_pdc").text()) +
+            (creditHrs7.get("fyp1")*$("#gp_fyp1").text());
+        case 8:
+            return (creditHrs8.get("mt")*$("#gp_mt").text()) +
+            (creditHrs8.get("sda")*$("#gp_sda").text()) +
+            (creditHrs8.get("fyp2")*$("#gp_fyp2").text());
+
+        default: return null;
+    }
 }
 
-function clearErrors() {
-    creditHours1.className = creditHours1.className.replace(" red-border", "");
-    creditHours2.className = creditHours2.className.replace(" red-border", "");
-    creditHours3.className = creditHours3.className.replace(" red-border", "");
-    creditHours4.className = creditHours4.className.replace(" red-border", "");
-    creditHours5.className = creditHours5.className.replace(" red-border", "");
-    creditHours6.className = creditHours6.className.replace(" red-border", "");
 
-    obtainedMarks1.className = obtainedMarks1.className.replace(" red-border", "");
-    obtainedMarks2.className = obtainedMarks2.className.replace(" red-border", "");
-    obtainedMarks3.className = obtainedMarks3.className.replace(" red-border", "");
-    obtainedMarks4.className = obtainedMarks4.className.replace(" red-border", "");
-    obtainedMarks5.className = obtainedMarks5.className.replace(" red-border", "");
-    obtainedMarks6.className = obtainedMarks6.className.replace(" red-border", "");
+function semListeners() {
+    for (const subj of creditHrs1.keys()) {
+        listenerSem(subj, 1);
+    }
+    for (const subj of creditHrs2.keys()) {
+        listenerSem(subj, 2);
+    }
+    for (const subj of creditHrs3.keys()) {
+        listenerSem(subj, 3);
+    }
+    for (const subj of creditHrs4.keys()) {
+        listenerSem(subj, 4);
+    }
+    for (const subj of creditHrs5.keys()) {
+        listenerSem(subj, 5);
+    }
+    for (const subj of creditHrs6.keys()) {
+        listenerSem(subj, 6);
+    }
+    for (const subj of creditHrs7.keys()) {
+        listenerSem(subj, 7);
+    }
+    for (const subj of creditHrs8.keys()) {
+        listenerSem(subj, 8);
+    }
 }
 
+function listenerSem(subject, semester) {
+    var marks_subj = `#marks_${subject}`;
+    var grade_subj = `#grade_${subject}`;
+    var gp_subj = `#gp_${subject}`;
+    var semester_gpa = `#gpa_${semester}`;
+
+    $(marks_subj).on("input", function() {
+        $(grade_subj).text(getGradeFromMarks($(marks_subj).val()));
+        $(gp_subj).text(getGPFromMarks($(marks_subj).val()));
+
+        // gpa
+        var gpa = (getQGradesForSem(semester)/getTotalCHrsForSem(semester)).toFixed(2);
+
+        if (isNaN(gpa)) {
+            gpa = "-";
+        }
+        $(semester_gpa).text(`GPA: ${gpa}`);
+
+        // cgpa
+        if ($(semester_gpa).text() != "GPA: -") {
+            var sumOfQGrades = 0;
+            var sumOfCHrs = 0;
+
+            for (var x=1; x<=8; x++) {
+                var gpa_sem = `#gpa_${x}`;
+
+                if ($(gpa_sem).text() != "GPA: -") {
+                    sumOfQGrades += getQGradesForSem(x);
+                    sumOfCHrs += getTotalCHrsForSem(x);
+                }
+            }
+
+            $("#gpa_cgpa").text(`CGPA: ${(sumOfQGrades/sumOfCHrs).toFixed(2)}`);
+        }
+
+    });
+}
+
+function getTotalCHrsForSem(semester) {
+    switch (semester) {
+        case 1: return totalCHrs1;
+        case 2: return totalCHrs2;
+        case 3: return totalCHrs3;
+        case 4: return totalCHrs4;
+        case 5: return totalCHrs5;
+        case 6: return totalCHrs6;
+        case 7: return totalCHrs7;
+        case 8: return totalCHrs8;
+        default: return null;
+    }
+}
+
+function getGradeFromMarks(marks) {
+    if (marks == "") return "-";
+    if (marks >= 80) return "A";
+    if (marks >= 75) return "B+";
+    if (marks >= 70) return "B";
+    if (marks >= 65) return "C+";
+    if (marks >= 55) return "C";
+    if (marks >= 50) return "D";
+    return "F";
+}
+
+function getGPFromMarks(marks) {
+    if (marks == "") return "-";
+    if (marks >= 80) return 4.0;
+    if (marks >= 75) return 3.5;
+    if (marks >= 70) return 3.0;
+    if (marks >= 65) return 2.5;
+    if (marks >= 55) return 2.0;
+    if (marks >= 50) return 1.5;
+    return 0;
+}
